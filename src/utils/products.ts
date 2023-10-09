354caaefbbe99c8f6e02f50c4bd2b9c8f058deb5
Drew Arocha <aarocha.aa@gmail.com>
2023-10-08 22:59:14 -0400

    updated about me page

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -2,37 +2,37 @@ import url from '../assets/GLASSES-DWEEB.jpeg'
 export const PRODUCTS = [
     {
         id: 1,
-        name: "iphone",
+        name: "Glasses 1",
         price: 999.0,
         img: url,
     },
     {
         id: 2,
-        name: "mac",
+        name: "Glasses 2",
         price: 1999.0,
         img: url,
     },
     {
         id: 3,
-        name: "switch",
+        name: "Glasses 3",
         price: 540.0,
         img: url,
     },
     {
         id: 4,
-        name: "dog",
+        name: "Glasses 4",
         price: 1.0,
         img: url,
     },
     {
         id: 5,
-        name: "grilled cheese",
+        name: "Glasses 5",
         price: 10000,
         img: url,
     },
     {
         id: 6,
-        name: "Glasses",
+        name: "Glasses 6",
         price: 500,
         img: url
     }
