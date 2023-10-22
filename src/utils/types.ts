b8b1098491a8cbbe4f018cf67e4efb2df4a6daca
Scead64 <tinor.jain@gmail.com>
2023-10-22 18:43:04 -0400

    Updated ViewOrder and types.ts     - Added shipping types to types for fututre convenience     - Changed name field in view ViewOrder     - Added axios posting to handle confirm

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -5,7 +5,26 @@ export default interface ProductType {
     img: string;
   }
 
-  export default interface CartType{
+export default interface CartType{
     product: ProductType;
     quantity: number;
+  }
+
+export default interface ShippingInfoType{
+    id: number;
+    first_name: string; 
+    last_name: string; 
+    email: string; 
+    phone_number: string; 
+    address: string; 
+    apt_suite_number: string;
+    city: string;
+    state: string;
+    zip_code: string;
+    card_first_name: string;
+    card_last_name: string;
+    card_number: string;
+    card_exp_date: string;
+    card_security_code: string;
+    card_zip_code: string;
   }
--- src/viewOrder.tsx
+++ src/viewOrder.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./viewOrder.css";
 import {useNavigate} from 'react-router-dom'
 import {useLocation} from "react-router-dom";
+import axios from "axios";
 //import {Purchase, order} from './purchase.tsx'
 // import order from "./paymentEntry";
 
@@ -9,8 +10,32 @@ const ViewOrder = () => {
   
   const location = useLocation();
   const navigate = useNavigate();
-  
+
   const handleConfirm = () => {
+    
+    // Send the shipping info to server
+    axios.post('http://127.0.0.1:8000/shipping_info/', {
+      // shipping_id: 1,
+      first_name: location.state.order.first_name,
+      last_name: location.state.order.last_name,
+      email: location.state.order.email,
+      phone_number: location.state.order.phone_number, 
+      address: location.state.order.address_1,
+      apt_suite_number: location.state.order.address_2,
+      city: location.state.order.city,
+      state: location.state.order.state,
+      zip_code: location.state.order.zip,
+      card_first_name: location.state.order.card_holder_first_name,
+      card_last_name: location.state.order.card_holder_last_name,
+      card_number: location.state.order.credit_card_number,
+      card_exp_date: location.state.order.expir_date,
+      card_security_code: location.state.order.cvc,
+      card_zip_code: location.state.order.card_postal
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error))
+
+    // Navigate to the confirmation page
     navigate('/confirm', {state: location.state.order});
   }
 
@@ -18,8 +43,10 @@ const ViewOrder = () => {
     <div className = "payment-container">
       <div className="payment-customer">
         <h1> Customer Information</h1>
-        <h2>First and Last Name </h2>
-        <h3>{location.state.order.card_holder_name}</h3>
+        <h2>First Name</h2>
+        <h3>{location.state.order.first_name}</h3>
+        <h2>Last Name</h2>
+        <h3>{location.state.order.last_name}</h3>
         <h2>Email</h2>
         <h3>{location.state.order.email}</h3>
         <h2>Phone Number</h2>
